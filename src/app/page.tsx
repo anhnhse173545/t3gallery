12b26e9bfcea9aad327f4d6f254d728aff0882c8
NguyenHoangAnhSE173545 <anhnhse173545@fpt.edu.vn>
2024-12-05 20:01:22 +0700

    db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockURLs = [
   "https://utfs.io/f/bKGrwwMj5D1jUzwMxdFqcitzG12LE4YSxWJjIHnlCKye65dr",
@@ -12,10 +13,18 @@ const mockImages = mockURLs.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main>
       <div className="flex flex-wrap gap-4">
+        {posts.map((post) => (
+          <div key={post.id} className="w-48">
+            {post.name}
+          </div>
+        ))}
         {mockImages.map((image) => (
           <div key={image.id} className="w-48">
             <img src={image.url} />
