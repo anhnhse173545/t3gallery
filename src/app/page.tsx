c0fd12149a959c8ce4956029162c3e4e9bdafa88
NguyenHoangAnhSE173545 <anhnhse173545@fpt.edu.vn>
2024-12-22 17:18:18 +0700

    update auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,37 @@
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 import Link from "next/link";
 import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
+async function Images() {
+  const images = await db.query.images.findMany();
+  return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image, index) => (
+        <div key={index} className="w-48">
+          <img src={image.url} />
+          <p>{image.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
 export default async function HomePage() {
   
 
   const images = await db.query.images.findMany();
 
+  
+
   return (
     <main>
-      <div className="flex flex-wrap gap-4">
-        {images.map((image, index) => (
-          <div key={index} className="w-48">
-            <img src={image.url} />
-            <p>{image.name}</p>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div>Please sign in to view the gallery</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
